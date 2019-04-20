68fa96ca29af235389c73fc7bb932834538e9804
stacyhanawa <stacyhanawa@gmail.com>
2019-04-20 09:50:52 -0700

    configured bar chart to size to highest rate

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
 fetch("https://api.exchangeratesapi.io/latest")
     .then(response => response.json())
     .then(data => {
-        console.log("Got the data!")
         console.log(data);
-        displayOutput(data)     
+        findHighest(data);
+        displayOutput(data);  
      });
-    
+
 let displaycurrency = [
             "USD",
             "AUD",
             "GBP",
             "BRL",
 ]
+     
+let highest = 0;
+
+function findHighest(data){
+    for (let currency of displaycurrency) {
+        let rate = data.rates[currency]   
+        if (rate > highest) {
+        highest = rate;
+        }
+    }
+}
     
 function displayOutput(data) {     
     let chart = document.querySelector(".BarChart");    
     chart.innerHTML = '';
     chart.innerHTML += `
-    <div class="BarChart-bar" style="height: ${1 * 20}%">
+    <div class="BarChart-bar" style="height: ${1/highest * 100}%">
         EUR<br>
         1.00
     </div>
     `;
+    
     for (let currency of displaycurrency) {
         let rate = data.rates[currency]
-        console.log(rate)
         chart.innerHTML += `
-            <div class="BarChart-bar" style="height: ${rate * 20}%">
+            <div class="BarChart-bar" style="height: ${rate/highest * 100}%">
                 ${currency}<br>
                 ${rate}
             </div>
-    `; 
+        `;
     }
 }
