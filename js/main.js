1c0fc977c87ac3f275a77c48858b3da09fd5c342
stacyhanawa <stacyhanawa@gmail.com>
2019-04-20 11:37:22 -0700

    added <select> drop-down function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-fetch("https://api.exchangeratesapi.io/latest")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        findHighest(data);
-        displayOutput(data);  
-     });
+function doFetch() {
+    let selected = document.getElementsByClassName("CurrencyChooser-select")[0];
+    let baseCurrency = selected.options[selected.selectedIndex].value;
+    fetch("https://api.exchangeratesapi.io/latest?base=" +baseCurrency)
+        .then(response => response.json())
+        .then(data => {
+            if (baseCurrency === "EUR") {
+                data.rates.EUR=1.00
+            }
+            console.log(data);
+            findHighest(data);
+            displayOutput(data);  
+         });
+}
 
 let displaycurrency = [
+            "EUR",
             "USD",
             "AUD",
             "GBP",
@@ -26,16 +34,9 @@ function findHighest(data){
     
 function displayOutput(data) {     
     let chart = document.querySelector(".BarChart");    
-    chart.innerHTML = '';
-    chart.innerHTML += `
-    <div class="BarChart-bar" style="height: ${1/highest * 100}%">
-        EUR<br>
-        1.00
-    </div>
-    `;
-    
+    chart.innerHTML = '';  
     for (let currency of displaycurrency) {
-        let rate = data.rates[currency]
+        let rate = data.rates[currency].toFixed(2)
         chart.innerHTML += `
             <div class="BarChart-bar" style="height: ${rate/highest * 100}%">
                 ${currency}<br>
@@ -44,3 +45,5 @@ function displayOutput(data) {
         `;
     }
 }
+
+doFetch()
